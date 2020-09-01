2bc971b9043e00578453546dfc5ec29202b55bfb
lrf <lrf@meitu.com>
2020-09-01 16:49:13 +0800

    style: 添加公共样式

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { GlobalStyle } from '../src/components/shared/global';
-import { addDecorator, addParameters } from '@storybook/react';
-import { withA11y } from '@storybook/addon-a11y';
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
+import React from "react";
+import { GlobalStyle } from "../src/components/shared/global";
+import { addDecorator, addParameters } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
 
 addParameters({
-  options: {
-    showRoots: true,
-  },
-  dependcies: {
-    withStoriesOnly: true,
+	options: {
+		showRoots: true,
+	},
+	dependencies: {
+		withStoriesOnly: true,
 		hideEmpty: true,
-  },
+	},
 });
-
 addDecorator(withA11y);
 addDecorator((story) => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
-))
+	<>
+		<GlobalStyle />
+		{story()}
+	</>
+));
