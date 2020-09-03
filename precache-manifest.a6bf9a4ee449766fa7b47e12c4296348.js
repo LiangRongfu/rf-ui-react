f6e1d96a7112df0792682b7c8e951204f57fa69e
LiangRongfu <1009224151@qq.com>
2020-09-03 08:43:25 +0000

    Deploying to gh-pages from  @ 3dbe15336284b23ec1de477e1fcba9706a743cfe 🚀

---
+++ precache-manifest.a6bf9a4ee449766fa7b47e12c4296348.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42176f8d45f357ab5594bc8e284fc7f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fd35008fffd5d158ab33.bundle.js"
+  },
+  {
+    "url": "runtime~main.fd35008fffd5d158ab33.bundle.js"
+  },
+  {
+    "url": "vendors~main.fd35008fffd5d158ab33.bundle.js"
+  },
+  {
+    "url": "vendors~main.fd35008fffd5d158ab33.bundle.js.LICENSE.txt"
+  }
+]);
